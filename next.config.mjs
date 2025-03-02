5d301d52efdc7c9cf2b016f149051ec43ef08ed3
nguyeenphan <phannguyenbh0902@gmail.com>
2025-03-02 23:05:44 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["utfs.io"],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+            },
+            {
+                protocol: 'https',
+                hostname: '**.ufs.sh',
+            },
+        ],
     },
 };
 
